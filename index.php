b13a737e24356157f5f3c20c43014f90ce904676
hnzzyagami <hnzzyagami@gmail.com>
2020-02-14 20:49:59 -0500

    modified:   index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>  
       <head>  
-           <title>Live Table Data Edit</title>  
+           <title>ipantry</title>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
