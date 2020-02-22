e0d3ef1358486acf2607aea65cb00b1200c71821
EC2 Default User <ec2-user@ip-172-31-29-226.ca-central-1.compute.internal>
2020-02-22 13:44:22 +0000

    v1.0

--- select.php
+++ select.php
@@ -2,7 +2,7 @@
 include_once 'db/conn.php';
 include_once 'db/dao.php';
 
-//$sql = "SELECT * FROM scanned_item GROUP BY scanned_txt ORDER BY lastop_datetime DESC";
+//select newest update item group by scanned_txt
 $sql = "select *
 from (select p.*,
              row_number() over (partition by scanned_txt order by lastop_datetime desc) as seqnum
@@ -12,23 +12,18 @@ where seqnum = 1
 order by lastop_datetime desc";
 $result = mysqli_query($connect, $sql);
 
-$count_total = get_total_count();
-
-$output = '';
-$output .= '  
-    <div class="table-boarderless">
-    <h4 align="center">            
-    <div align="middle">
-         <font color=blue>' . $count_total["undecided"] . "</font>/<font color=green>" . $count_total["left"] . '</font>
-         <font color=red>' . $count_total["trashed"] . "</font>/<font color=green>" . $count_total["stocked"] . "</font>/<font color=blue>" . $count_total["scanned"]  . '</font>
-         </div>
-    </h4>       
-    <table class="table table-boarderless" >
+$output = '  
+    <div class="table-boarderless">     
+        <table class="table table-boarderless" >
           ';
 if (mysqli_num_rows($result) > 0) {
 
     while ($row = mysqli_fetch_array($result)) {
+
         $count = get_count($row["scanned_txt"]);
+        $undecided = $count["undecided"];
+        $left = $count["left"];
+
         //$img_height = fix_image_ratio(100, $row["image_thumb_url"]);
         if ($row["lastop_datetime"] = $row["stock_datetime"])
             $color = "#5cb85c";
@@ -39,43 +34,34 @@ if (mysqli_num_rows($result) > 0) {
 
         $output .= '  
                 <tr>  
-                     <td class="image_thumb_url " data-id4="' . $row["id"] . '"><img src=' . $row["image_thumb_url"] . ' class="img-fluid img-thumbnail"></img>
-                         
-                     </td>
-                     <td>
-                     <div class="scanned_txt" data-id1="' . $row["id"] . '" contenteditable>' . $row["scanned_txt"] . '</div>
-                     <div class="product_name" data-id2="' . $row["id"] . '" contenteditable>' . $row["product_name"] . '</div>
-                     <div class="brand_name" data-id3="' . $row["id"] . '" contenteditable>' . $row["brand_name"] . '</div>       
-                     ';
+                    <td>
+                        <a href="#" class="notification">
+                            <img src=' . $row["image_thumb_url"] . ' class="img-fluid img-thumbnail"></img>';
 
-        $output .= '
-                     <div><font color=blue>' . $count["undecided"] . '</font>/<font color=green>' . $count["left"] . '</font>
-                         <font color=red>' . $count["trashed"] . "</font>/<font color=green>" . $count["stocked"] . "</font>/<font color=blue>" . $count["scanned"]  . '</font>
-                         </div> ';
+        if ($undecided)
+            $output .=   '<span class="badge_right"> ';
+        else
+            $output .=   '<span class="badge_right invisible"> ';
+        $output .= $undecided . '</span>
+                            <span class="badge_left">' . $left . '</span>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="scanned_txt" data-id1="' . $row["id"] . '" contenteditable>' . $row["scanned_txt"] . '</div>
+                        <div class="product_name" data-id2="' . $row["id"] . '" contenteditable>' . $row["product_name"] . '</div>
+                        <div class="brand_name" data-id3="' . $row["id"] . '" contenteditable>' . $row["brand_name"] . '</div>  
+                  ';
         //if ($count["undecided"] > 0) {
         // if (($row["trash_datetime"] == NULL) and ($row["stock_datetime"] == NULL)) {
         $output .= '<button ';
-        if (($count["undecided"] < 1) or ($count["left"] < 1))
-            $output .= 'disabled ';
+        if (($undecided < 1) or ($left < 1))
+            $output .= 'class="invisible" ';
         $output .= 'type="button" name="delete_btn" data-id6="' . $row["scanned_txt"] . '" class="btn btn-s btn-danger btn_delete" >Trash</button>';
         $output .= '   ';
         $output .= '<button ';
         if ($count["undecided"] < 1)
-            $output .= 'disabled ';
-        $output .= 'type="button" name="stock_btn" data-id9="' . $row["scanned_txt"] . '" class="btn btn-s btn-success btn_stock" >Stock(' . $count["left"] . ')</button>';
-
-
-        // } else if ($row["trash_datetime"] != NULL) {
-        //     $output .= '<div class="trash_datetime" style="color:#c9302c;">' . $row["trash_datetime"] . '</div>';
-        // } else if ($row["stock_datetime"] != NULL) {
-        //     $output .= '<div class="stock_datetime" style="color:#5cb85c;">' . $row["stock_datetime"] . '</div>';
-        //}
-
-        // if ($row["stock_datetime"] != NULL) {
-        //     $output .= '<button type="button" name="stock_btn" data-id7="' . $row["id"] . '" class="btn btn-s btn-success btn_stock" disabled>In Pantry</button>';
-        // } else {
-        //     $output .= '<button type="button" name="stock_btn" data-id9="' . $row["id"] . '" class="btn btn-s btn-success btn_stock" >To Pantry</button>';
-        // }
+            $output .= 'class="invisible" ';
+        $output .= 'type="button" name="stock_btn" data-id9="' . $row["scanned_txt"] . '" class="btn btn-s btn-success btn_stock" >Stock</button>';
         $output .= ' </td>
                
             </tr>  
